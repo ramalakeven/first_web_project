9ef435c26f3be0519f1eb73a070049059ccac6fd
ramalakeven <143877366+ramalakeven@users.noreply.github.com>
2026-06-16 23:37:44 +0300

    Task 7 ready.

--- my_project/config/urls.py
+++ my_project/config/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import index, about
+from pages.views import index, about, article_detail
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', index, name='home'),
-     path(
-        'about/',
-        about,
-        name='about'
+    path('about/', about, name='about'),
+
+    path(
+        'article/<int:pk>/',
+        article_detail,
+        name='article_detail'
     ),
 ]
--- my_project/pages/views.py
+++ my_project/pages/views.py
@@ -1,22 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 
 def index(request):
 
+    items = Article.objects.all()
+
     context = {
         'title': 'Главная страница',
-        'welcome_text': 'Добро пожаловать на наш сайт!'
+        'welcome_text': 'Добро пожаловать на наш сайт!',
+        'items': items,
     }
 
-    return render(
-        request,
-        'pages/index.html',
-        context
-    )
-
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(
         request,
         'pages/about.html'
+    )
+
+def article_detail(request, pk):
+
+    article = get_object_or_404(
+        Article,
+        pk=pk
+    )
+
+    context = {
+        'article': article
+    }
+
+    return render(
+        request,
+        'pages/detail.html',
+        context
     )
