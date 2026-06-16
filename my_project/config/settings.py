853fee1d2d1a2ddf483b99abaf84028e6313f1ee
ramalakeven <143877366+ramalakeven@users.noreply.github.com>
2026-06-17 02:05:21 +0300

    Task 11 partly?ready.

--- my_project/config/settings.py
+++ my_project/config/settings.py
@@ -130,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- my_project/config/urls.py
+++ my_project/config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from pages.views import (
     index,
@@ -58,4 +60,10 @@ urlpatterns = [
         register,
         name='register'
     ),
+    
 ]
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
